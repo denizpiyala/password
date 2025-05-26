bd0b6ea7432a7aad582f06ed1a21444bb670e822
Deniz Piyala <denizpiyala035@gmail.com>
2025-05-26 15:11:49 +0300

    feat: update app component to include modify function and adjust template bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'password';
-  password:string = 'deniz';
-
-
-  buttonClick() {
-    this.password = "deniz piyala"
-    console.log("event bindind");
-  }
-
-  getPassword()
-  {
-    return this.password;
-  }
-  getName()
-  {
-    return 'panda'
-  }
+modify() {
+throw new Error('Method not implemented.');
 }
+  title = 'password';
+  buttonClick() {
+    console.log("event binding i öğreniyorum");
+  } //fonksiyon ismiyele yapmak istediğimiz işlemi yazıyoruz.
+}
+ 
+  
+
