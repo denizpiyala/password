1f1bb44c6abf64556da005eb8c96a3f16e0ac949
Deniz Piyala <denizpiyala035@gmail.com>
2025-05-27 18:48:45 +0300

    feat: update app component to enhance input handling and clean up unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,40 @@ import { RouterOutlet } from '@angular/router';
 })
 
 export class AppComponent {
-  title = 'password';
-  
-  password:string = 'deniz';
+  lenght:number = 0;
+  includeLetters:boolean = false;
+  includeNumbers:boolean = false;
+  includeSymbols:boolean = false;
+
+  modifyLenght(value: string) {
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue))
+      {
+      debugger;
+      this.lenght = parsedValue;
+
+    }
+  }
+
+  modifyLetters()
+  {
+    this.includeLetters = !this.includeLetters;
+
+  }
+  modifyNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  modifySybols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
   buttonClick() {
-    this.password = 'diniz piyala';
-     
-    console.log("event binding i öğreniyorum");
-  } //fonksiyon ismiyele yapmak istediğimiz işlemi yazıyoruz.
-  getPassword()
-  {
-    return this.password;
-  }
 
-  getName()
-  {
-    return 'Hilal';
-  }
-
-
+    console.log(`değerlerim,
+      Letters: ${this.includeLetters}
+      Numbers: ${this.includeNumbers}
+      Symbols: ${this.includeSymbols}
+      `);
+  } 
 }
  
   
